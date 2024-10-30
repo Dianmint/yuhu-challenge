74c1073462f6759d4d3ebc5dd13e837372248cc0
Diana Aguilar Jimenez <diana.aguilar@yuhu.mx>
2024-10-29 19:54:58 -0600

    celery.config: Add Celery configuration

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -129,3 +129,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Broker
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+
+# Configuracion gmail
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'       
+EMAIL_PORT = 587                    
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'dian.job.pruebas@gmail.com'   
+EMAIL_HOST_PASSWORD = 'mdlt pchc mlgh noim'     
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
